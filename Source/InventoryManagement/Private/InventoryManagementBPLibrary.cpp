025ded0db5006cb622098afe337f594cceca0937
holy-bit <luischamarro97@gmail.com>
2020-04-09 21:18:40 +0200

    Plugin Created

---
+++ Source/InventoryManagement/Private/InventoryManagementBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "InventoryManagementBPLibrary.h"
+#include "InventoryManagement.h"
+
+UInventoryManagementBPLibrary::UInventoryManagementBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UInventoryManagementBPLibrary::InventoryManagementSampleFunction(float Param)
+{
+	return -1;
+}
+
